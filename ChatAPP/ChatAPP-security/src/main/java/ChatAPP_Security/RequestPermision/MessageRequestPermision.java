ca33ea0b5d390c6ccb1265819857bca91ef1e5e2
anton <anton@LenovoPCPrace>
2024-01-28 16:29:14 +0100

    MessageRequestPermision-have to be implement

--- ChatAPP/ChatAPP-security/src/main/java/ChatAPP_Security/RequestPermision/MessageRequestPermision.java
+++ ChatAPP/ChatAPP-security/src/main/java/ChatAPP_Security/RequestPermision/MessageRequestPermision.java
@@ -5,6 +5,7 @@ public interface MessageRequestPermision {
 	/**Metod verify, if id is same, and eventually make security consequences  */
 	public void verifyMessageOwnership(long messageOwnerID,long SenderID,long WebSocketSessionOwnerID);
 	
+	
 	/**Metod verify if sender of message is same as sender from WebSocketSession
 	 * ,Also, metod verify if user has permision to write into chat  
 	 * */
