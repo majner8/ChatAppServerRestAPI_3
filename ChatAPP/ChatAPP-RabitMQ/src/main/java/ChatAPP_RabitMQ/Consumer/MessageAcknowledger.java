e8321c51bf36339b401a7bb26002073f7206d5c3
anton <anton@LenovoPCPrace>
2024-02-05 16:37:00 +0100

    after finish Consumer

--- ChatAPP/ChatAPP-RabitMQ/src/main/java/ChatAPP_RabitMQ/Consumer/MessageAcknowledger.java
+++ ChatAPP/ChatAPP-RabitMQ/src/main/java/ChatAPP_RabitMQ/Consumer/MessageAcknowledger.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public interface MessageAcknowledger {
 
-	public void AckMessage(String sessionID,String messageID) throws IOException;
-	public void NackMessage(String sessionID,String messageID) throws IOException;
+	public void AckMessage(String sessionID,String messageID) ;
+	public void NackMessage(String sessionID,String messageID) ;
 
 	
 }
--- ChatAPP/ChatAPP-RabitMQ/src/main/java/ChatAPP_RabitMQ/Consumer/UnacknowledgedMessagesService.java
+++ ChatAPP/ChatAPP-RabitMQ/src/main/java/ChatAPP_RabitMQ/Consumer/UnacknowledgedMessagesService.java
@@ -1,6 +1,7 @@
 package ChatAPP_RabitMQ.Consumer;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +10,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import com.rabbitmq.client.Channel;
+
+import chatAPP_CommontPart.Log4j2.Log4j2;
 
 @Component
 @Order(1)
@@ -23,8 +26,45 @@ public class UnacknowledgedMessagesService implements UnAcknowledgeMessageListMa
 	}
 
 	@Override
-	public void stopConsume(String userdeviceID, boolean DoesDeviceDisconect) {
+	public void stopConsume(String userdeviceID, boolean DoesDeviceDisconect)  {
+		Map<String,Messages> mes;
 		//have to nack all messages
+		synchronized(this.listOfMessage) {
+		 mes=this.listOfMessage.get(userdeviceID);
+		 if(mes==null) {
+			 Log4j2.log.warn(Log4j2.MarkerLog.RabitMQ.getMarker(),
+					 String.format("client connection, ID: %s was end, but Appropriate RabitMQ object has been removed before. ",userdeviceID ));
+			 return;
+		 }
+		 this.listOfMessage.remove(userdeviceID);		
+		 }
+		
+		synchronized(mes) {
+			Collection<Messages> x=mes.values();
+			for(Messages V:x) {
+				if(V.isWasMessageAlreadyAcknowledged()) {
+					continue;
+				}
+				try {
+					V.getRabitChannel().basicNack(V.getDeliveryTag(), false, V.isShouldBeMessageRequiredAfterExpiration());
+				} catch (IOException e) {
+					Log4j2.log.error(Log4j2.MarkerLog.RabitMQ.getMarker(),String.format("Error during NackMessage, ID: %s deliveryTag: %s %s %s", V.getMessageID(),V.getDeliveryTag(),System.lineSeparator(),e));
+					return;
+				}
+				
+			}
+			
+			mes.forEach((K,V)->{
+				if(V.isWasMessageAlreadyAcknowledged()) {
+					return;
+				}
+				try {
+					V.getRabitChannel().basicNack(V.getDeliveryTag(), false, V.isShouldBeMessageRequiredAfterExpiration());
+				} catch (IOException e) {
+					
+				}
+			});
+		}
 		
 	}
 		public static final class Messages{
@@ -71,7 +111,7 @@ public class UnacknowledgedMessagesService implements UnAcknowledgeMessageListMa
 	
 	
 	@Override
-	public void AckMessage(String sessionID, String messageID) throws IOException {
+	public void AckMessage(String sessionID, String messageID) {
 		
 		Map<String,Messages> message=this.listOfMessage.get(sessionID);
 		Messages mes=message.get(messageID);
@@ -89,7 +129,7 @@ public class UnacknowledgedMessagesService implements UnAcknowledgeMessageListMa
 				mes.getRabitChannel().basicAck(mes.getDeliveryTag(), false);
 				}
 				catch(IOException e) {
-					throw e;
+					Log4j2.log.error(Log4j2.MarkerLog.RabitMQ.getMarker(),String.format("Error during AckMessage, ID: %s deliveryTag: %s %s %s", mes.getMessageID(),mes.getDeliveryTag(),System.lineSeparator(),e));
 				}
 				finally {
 					message.remove(messageID);
@@ -100,7 +140,7 @@ public class UnacknowledgedMessagesService implements UnAcknowledgeMessageListMa
 	}
 
 	@Override
-	public void NackMessage(String sessionID, String messageID) throws IOException {
+	public void NackMessage(String sessionID, String messageID) {
 		Map<String,Messages> message=this.listOfMessage.get(sessionID);
 		Messages mes=message.get(messageID);
 		
@@ -118,7 +158,7 @@ public class UnacknowledgedMessagesService implements UnAcknowledgeMessageListMa
 			mes.getRabitChannel().basicNack(mes.getDeliveryTag(), false,mes.isShouldBeMessageRequiredAfterExpiration());
 			}
 			catch(IOException e) {
-				throw e;
+				Log4j2.log.error(Log4j2.MarkerLog.RabitMQ.getMarker(),String.format("Error during NackMessage, ID: %s deliveryTag: %s %s %s", mes.getMessageID(),mes.getDeliveryTag(),System.lineSeparator(),e));
 			}
 			finally {
 				message.remove(messageID);
