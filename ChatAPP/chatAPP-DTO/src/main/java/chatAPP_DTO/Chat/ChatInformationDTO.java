eed0675e66875bed597227da02449478c94689f4
anton <anton@LenovoPCPrace>
2024-03-16 12:03:22 +0100

    16.3

--- ChatAPP/chatAPP-DTO/src/main/java/chatAPP_DTO/Chat/ChatInformationDTO.java
+++ ChatAPP/chatAPP-DTO/src/main/java/chatAPP_DTO/Chat/ChatInformationDTO.java
@@ -78,7 +78,7 @@ public  class ChatInformationDTO implements DTO{
 	@Override
 	public String getMessageID() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.getChatID();
 	}
 
 }
